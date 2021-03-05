5642d223fb8f82da712a555e72134a819d54cd2c
lucasfavo <lucashp_63@hotmail.com>
2021-03-05 00:57:04 +0000

    implementação de interface

--- src/main/java/br/com/impacta/lab/Pessoa.java
+++ src/main/java/br/com/impacta/lab/Pessoa.java
@@ -4,5 +4,7 @@ public interface Pessoa {
 
 	public String falarDocumento();
 	public String getTipo();
+  public void setDocumento(String documento);
+  public void setTipo(String tipo);
 	
 }
---
+++ src/main/java/br/com/impacta/lab/PessoaJuridica.java
@@ -0,0 +1,23 @@
+package br.com.impacta.lab;
+
+public class PessoaJuridica implements Pessoa {
+
+  private String documento;
+  private String tipo;
+
+  public void setDocumento(String documento){
+    this.documento = documento;
+  }
+
+  public void setTipo(String tipo){
+  this.tipo = tipo;
+  }
+  public String falarDocumento(){
+    return this.documento;
+  }
+
+  public String getTipo(){
+    return this.tipo;
+  }
+
+}
